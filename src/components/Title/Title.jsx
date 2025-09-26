813b4c5b5c7e2d8b48f5a07fe63cd993d7b0332d
naomiperez <naomisperez99@gmail.com>
2025-09-25 18:04:39 -0700

    chore(dependencies): replace unmaintained 'react-reveal' with 'react-awesome-reveal'

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Fade from 'react-reveal/Fade';
+import { Fade } from 'react-awesome-reveal';
 import PropTypes from 'prop-types';
 
 const Title = ({ title }) => (
