5cb40adaa2fcc9f0e0a74737b6180352a345a90a
naomiperez <naomisperez99@gmail.com>
2026-02-14 18:58:18 -0800

    feat(media): enhance media links with hand-drawn style and animations

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,19 +1,49 @@
-import React from 'react'
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
-export default class Media extends React.Component {
-
-    render() {
-        return (
-            <div className="media-grid">
-                <a href="https://www.linkedin.com/in/naomisp/" target="_blank" className="media">
-                    <StaticImage src="../images/linkedin.png" width="100" alt="LinkedIn" rel="noreferrer" />
-                </a>
-                <a href="https://github.com/naomiperez" target="_blank" className="media" >
-                    <StaticImage src="../images/github.png" width="100" alt="GitHub" />
-                </a>
-                <a href="https://drive.google.com/file/d/1DaGb4CdBtDyaLStjQgOWjLwSFy5vQoPr/view?usp=sharing" target="_blank" className="media" ><StaticImage src="../images/resume.png" width="90" alt="Resume" rel="noreferrer"></StaticImage></a>
-            </div>
-        );
-    }
+export default function Media() {
+  return (
+    <div className="media-grid media-grid--hand-drawn" aria-label="Links: LinkedIn, GitHub, Resume">
+      <a
+        href="https://www.linkedin.com/in/naomisp/"
+        target="_blank"
+        rel="noreferrer"
+        className="media media__link"
+      >
+        <span className="media__icon-wrap">
+          <StaticImage
+            src="../images/linkedin.png"
+            width="100"
+            alt="LinkedIn"
+            className="media__icon"
+          />
+        </span>
+      </a>
+      <a
+        href="https://github.com/naomiperez"
+        target="_blank"
+        rel="noreferrer"
+        className="media media__link"
+      >
+        <span className="media__icon-wrap">
+          <StaticImage
+            src="../images/github.png"
+            width="100"
+            alt="GitHub"
+            className="media__icon"
+          />
+        </span>
+      </a>
+      <a
+        href="https://drive.google.com/file/d/1DaGb4CdBtDyaLStjQgOWjLwSFy5vQoPr/view?usp=sharing"
+        target="_blank"
+        rel="noreferrer"
+        className="media media__link"
+      >
+        <span className="media__icon-wrap">
+          <StaticImage src="../images/resume.png" width="90" alt="Resume" className="media__icon" />
+        </span>
+      </a>
+    </div>
+  );
 }
